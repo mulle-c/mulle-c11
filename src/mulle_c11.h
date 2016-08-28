90653efaf4651aef16822c5e353323061834b621
Nat! <nat@mulle-kybernetik.com>
2016-08-28 21:09:17 +0200

    add more __attribute__ macros

--- src/mulle_c11.h
+++ src/mulle_c11.h
@@ -14,13 +14,39 @@
 // stuff only clang and gcc understands
 //
 #if defined( __clang__) || defined( __GNUC__)
+
 # define MULLE_C_ALWAYS_INLINE         __attribute__(( always_inline))
-# define MULLE_C_CONST_VALUE_RETURN    __attribute__(( const))
+# define MULLE_C_NEVER_INLINE          __attribute__(( noinline))
+
+# define MULLE_C_CONST_RETURN          __attribute__(( const))
+# define MULLE_C_NO_RETURN             __attribute__(( noreturn))
+# define MULLE_C_NON_NULL_RETURN       __attribute__(( returns_nonnull))
+
 # define MULLE_C_CONSTRUCTOR           __attribute__(( constructor))
+
+// some composites
+# define MULLE_C_CONST_NON_NULL_RETURN                 __attribute__(( const, returns_nonnull))
+# define MULLE_C_ALWAYS_INLINE_NON_NULL_RETURN         __attribute__(( always_inline, returns_nonnull))
+# define MULLE_C_ALWAYS_INLINE_NON_NULL_CONST_RETURN   __attribute__(( always_inline, returns_nonnull, const))
+
+
 #else
+
+
 # define MULLE_C_ALWAYS_INLINE
-# define MULLE_C_CONST_VALUE_RETURN
+# define MULLE_C_NEVER_INLINE
+
+# define MULLE_C_CONST_RETURN
+# define MULLE_C_NO_RETURN
+# define MULLE_C_NON_NULL_RETURN
+
 # define MULLE_C_CONSTRUCTOR
+
+# define MULLE_C_CONST_NON_NULL_RETURN
+# define MULLE_C_ALWAYS_INLINE_NON_NULL_RETURN
+# define MULLE_C_ALWAYS_INLINE_NON_NULL_CONST_RETURN
+
 #endif
+
 
 #endif
