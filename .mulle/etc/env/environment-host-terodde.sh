5b0b91d521be970fe3d5ebed539157bf218d57c2
Nat! <nat@codeon.de>
2020-03-26 13:57:40 +0100

    added missing files

---
+++ .mulle/etc/env/environment-host-terodde.sh
@@ -0,0 +1,6 @@
+#
+#
+#
+export MULLE_FETCH_SEARCH_PATH="/home/src/srcO/mulle-c:/home/src/srcO/mulle-core:/home/src/srcO/mulle-concurrent:/home/src/srcO/mulle-objc:/home/src/srcO/mulle-objc-ports:/home/src/srcO/MulleFoundation:/home/src/srcO/MulleWeb:/home/src/srcO/MulleScion:/home/src/srcM/MulleEOF/MulleEOControl:/home/src/srcM/MulleEOF/MulleEOAccess:/home/src/srcM/MulleEOF/MulleEOAInterface:/home/src/srcM/MulleEOF/MulleEOAdaptor:${MULLE_FETCH_SEARCH_PATH}"
+
+
