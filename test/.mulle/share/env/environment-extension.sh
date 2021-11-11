d3699f7bf2e4f1bc35b344a0e71b7c03d58a3d3a
Nat! <nat@mulle-kybernetik.com>
2021-11-12 00:17:55 +0100

    update mulle-sde, add .gitattributes if needed

--- test/.mulle/share/env/environment-extension.sh
+++ test/.mulle/share/env/environment-extension.sh
@@ -5,8 +5,7 @@ export MULLE_MATCH_FIND_NAMES="*.stdout,*.stdin,*.stderr,*.errors,*.ccdiag"
 
 
 #
-# Make project to test discoverable via MULLE_FETCH_SEARCH_PATH
-# We assume we are in ${PROJECT_DIR}/test so modify search path to add ../..
+# Make project to test discoverable via MULLE_FETCH_SEARCH_PATH We assume we are in ${PROJECT_DIR}/test so modify search path to add ../..
 #
 export MULLE_FETCH_SEARCH_PATH="${MULLE_FETCH_SEARCH_PATH}:${MULLE_VIRTUAL_ROOT}/../.."
 
