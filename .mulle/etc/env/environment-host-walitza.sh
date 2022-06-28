303d32ef32992fd65b7a621a1906fae2789c1712
Nat! <nat@mulle-kybernetik.com>
2022-06-29 01:16:26 +0200

    prerelease

--- .mulle/etc/env/environment-host-walitza.sh
+++ .mulle/etc/env/environment-host-walitza.sh
@@ -1,6 +1,6 @@
 #
 #
 #
-export MULLE_FETCH_SEARCH_PATH="/Volumes/Source/srcM/MulleEOF/MulleEOAdaptor:/Volumes/Source/srcM/MulleEOF/MulleEOInterface:/Volumes/Source/srcM/MulleEOF/MulleEOAccess:/Volumes/Source/srcM/MulleEOF/MulleEOControl:/Volumes/Source/srcO/MulleWeb:/Volumes/Source/srcO/MulleFoundation:/Volumes/Source/srcO/mulle-objc-ports:/Volumes/Source/srcO/mulle-objc:/Volumes/Source/srcO/mulle-core:/Volumes/Source/srcO/mulle-concurrent:/Volumes/Source/srcO/mulle-c:${MULLE_FETCH_SEARCH_PATH}"
+export MULLE_FETCH_SEARCH_PATH="${MULLE_FETCH_SEARCH_PATH}:/Volumes/Source/srcO/mulle-c:/Volumes/Source/srcO/mulle-concurrent:/Volumes/Source/srcO/mulle-core:/Volumes/Source/srcO/mulle-objc:/Volumes/Source/srcO/mulle-objc-ports:/Volumes/Source/srcO/MulleFoundation:/Volumes/Source/srcO/MulleWeb:/Volumes/Source/srcO/MulleUI:/Volumes/Source/srcM/MulleEOF/MulleEOControl:/Volumes/Source/srcM/MulleEOF/MulleEOAccess:/Volumes/Source/srcM/MulleEOF/Adaptors:/Volumes/Source/srcM/MulleEOF/MulleEOInterface:/Volumes/Source/srcC/hdt_master/Frameworks"
 
 
