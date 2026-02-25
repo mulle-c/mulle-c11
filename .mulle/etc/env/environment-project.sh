0f3bbd927a8c40b876b00e2128a9cabeae513480
Nat! <nat@mulle-kybernetik.com>
2026-02-25 18:15:13 +0100

    pruned most of what mulle-c/c-developer provided as we dont need it (only what the toolchains file)

--- .mulle/etc/env/environment-project.sh
+++ .mulle/etc/env/environment-project.sh
@@ -37,6 +37,30 @@ export PROJECT_SOURCE_DIR="src"
 #
 #
 #
-export PROJECT_ASSET_DIR="asset"
+export PROJECT_ASSET_DIR="DEFAULT"
+
+
+#
+#
+#
+export TEST_PROJECT_NAME="mulle-c11"
+
+
+#
+#
+#
+export PROJECT_LANGUAGE="c"
+
+
+#
+#
+#
+export PROJECT_DIALECT="c"
+
+
+#
+#
+#
+export PROJECT_EXTENSIONS="c"
 
 
---
+++ .mulle/share/env/environment-extension.sh
@@ -0,0 +1,36 @@
+#
+# Reset to empty
+#
+export MULLE_SDE_REFLECT_CALLBACKS="sourcetree:filesystem"
+
+
+#
+# Used by `mulle-match find` to speed up the search.
+#
+export MULLE_MATCH_FILENAMES="config*:*.h:*.inc:*.c:*.md:*.svg:*.png:*.jpg:*.mp3:*.wav:*.ogg:CMakeLists.txt:*.cmake"
+
+
+#
+# Used by `mulle-match find` to locate files
+#
+export MULLE_MATCH_PATH=".mulle/etc/sourcetree:${PROJECT_SOURCE_DIR}:${PROJECT_ASSET_DIR}:CMakeLists.txt:cmake"
+
+
+#
+# Used by `mulle-match find` to ignore boring subdirectories like .git
+#
+export MULLE_MATCH_IGNORE_PATH=""
+
+
+#
+# mulle-c and mulle-objc projects have an actual latest tag, so don't resolve
+#
+export MULLE_SOURCETREE_RESOLVE_TAG="NO"
+
+
+#
+# tell mulle-sde to keep files protected from read/write changes
+#
+export MULLE_SDE_PROTECT_PATH="cmake/share"
+
+
