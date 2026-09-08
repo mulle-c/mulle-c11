16230e93b4bb025ff72bdb6fac29b8782b1b10eb
Nat! <nat@mulle-kybernetik.com>
2026-09-08 13:19:15 +0200

    ci: set MULLE_SDE_REFLECT_CALLBACKS=NONE
    A fresh checkout (CI) lacks the generated .mulle/share/monitor callbacks, so
    mulle-sde fetch failed with 'sourcetree-callback not found'. Disabling reflect
    callbacks (supported by mulle-sde 3.9.1+) fixes the fetch step. This project
    does not rely on reflect callbacks at fetch time.

--- .mulle/etc/env/environment-global.sh
+++ .mulle/etc/env/environment-global.sh
@@ -40,3 +40,9 @@ export MULLE_MATCH_TO_C_RUN="DISABLE"
 
 
 
+#
+#
+#
+export MULLE_SDE_REFLECT_CALLBACKS="NONE"
+
+
--- .mulle/etc/env/environment-project.sh
+++ .mulle/etc/env/environment-project.sh
@@ -64,3 +64,9 @@ export PROJECT_DIALECT="c"
 export PROJECT_EXTENSIONS="c"
 
 
+#
+#
+#
+
+
+
